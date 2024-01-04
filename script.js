543ff28e339b1e41faf6ab6b9fda046b8b755d5d
Rayhona <rayhonamirzoyeva.ru>
2024-01-04 15:55:59 +0500

    sdfghj

--- script.js
+++ script.js
@@ -63,3 +63,63 @@ console.log(typeof arr);
 //console.log(5*5);
 //document.body.innerHTML = firstNumber
 
+
+  //? Operatorlar
+
+  let a = 30;
+  
+  a = 40;
+ 
+  let b = 10;
+
+  //? Addition
+  let c = a + b;
+
+  let str1 = "salom";
+
+  let strUnd = "nimadir" ;
+  console.log(str1 - strUnd);
+
+  //? MINUS
+
+  console.log("result", a-b );
+
+  //? Multipliction
+
+  let resMultiple = a*b;
+
+  console.log(resMultiple);
+
+  // ? Division
+
+  let div = a/b;
+
+  console.log(div);
+
+  // ?Qoldiqli bo`lish
+
+  console.log(45 % b);
+  let d = 30;
+   
+  // d = d+ a; === d += a;
+  d+=a; // d =d+a;
+
+  // string
+let z = 20;
+
+//number
+let x =20;
+
+//tenglik
+//==
+//===
+
+// ?emas manosid keladi 
+
+console.log(z < x);
+console.log(z !==x);
+
+// ? Darajaga kutarish
+console.log(z**3);
+
+
