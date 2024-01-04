2c2c280883a4e4c1c47c959d90adeb9ac92e6608
Rayhona <rayhonamirzoyeva.ru>
2024-01-04 15:04:51 +0500

    sdfghjkl

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("salom dunyo");
+
+let age ="fsyhwqfdyu";
+
+//CamelCase
+let colorCar ="red"
+
+// Data Type
+// Malumot turi
+
+//? STRING
+let str = "salom"
+let str2 = "dunyo"
+
+let str3 = 'Nima gap'
+let longStr = "Mening ismim 'Rayhona' "
+
+//? Number
+let firstNumber = 4
+let secondNumber = 10
+ let strNum ="4"
+
+ //Boolean
+let isTrue =true
+let isFalse = false
+
+//?Undefined
+
+let und;
+
+console.log("und");
+
+//?Null 
+let isNull = null;
+console.log(isNull);
+
+//?TypeOf
+console.log(typeof isTrue);
+
+//? Object
+
+let shaxzod ={
+    
+}
+
